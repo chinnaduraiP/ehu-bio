db3c52e35324ed72439d9c8baede5d88dbbaec21
gorka.prieto@gmail.com <gorka.prieto@gmail.com@f831f5fe-2d2f-98d9-2631-73cc1f612daa>
2013-09-20 17:28:08 +0000

    Included wregex GAE project

---
+++ Projects/java/wregex/src/es/ehu/grk/wregex/gae/ServletListenner.java
@@ -0,0 +1,15 @@
+package es.ehu.grk.wregex.gae;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+
+public class ServletListenner implements HttpSessionListener {
+	@Override
+	public void sessionCreated(HttpSessionEvent arg0) {						
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+	}	
+}
