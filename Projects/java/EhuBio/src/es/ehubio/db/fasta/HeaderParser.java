d4c7491cd30e7fd36f9760ed8940d0f4ba60526e
gorka.prieto@gmail.com <gorka.prieto@gmail.com@f831f5fe-2d2f-98d9-2631-73cc1f612daa>
2014-06-03 14:49:09 +0000

    Published PAnalyzer v1.1 which: - Includes support for mzIdentML 1.2.0 - Also reports sprectra information in the HTML - Some bugs fixed

--- Projects/java/EhuBio/src/es/ehubio/db/fasta/HeaderParser.java
+++
@@ -1,9 +0,0 @@
-package es.ehubio.db.fasta;
-
-public interface HeaderParser {
-	public boolean parse( String header );
-	public String getAccession();
-	public String getDescription();
-	public String getProteinName();
-	public String getGeneName();
-}
