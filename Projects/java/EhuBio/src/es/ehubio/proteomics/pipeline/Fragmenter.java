484e95557c4d86deac9b0f9c21be645fdb2c6348
gorka.prieto@gmail.com <gorka.prieto@gmail.com@f831f5fe-2d2f-98d9-2631-73cc1f612daa>
2014-11-20 17:02:43 +0000

    Continued fragment ion support for MSF files

--- Projects/java/EhuBio/src/es/ehubio/proteomics/pipeline/Fragmenter.java
+++ Projects/java/EhuBio/src/es/ehubio/proteomics/pipeline/Fragmenter.java
@@ -5,12 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import es.ehubio.model.Aminoacid;
 import es.ehubio.proteomics.FragmentIon;
+import es.ehubio.proteomics.IonType;
 import es.ehubio.proteomics.Masses;
 import es.ehubio.proteomics.Peptide;
 import es.ehubio.proteomics.Psm;
 import es.ehubio.proteomics.Ptm;
+import es.ehubio.proteomics.Spectrum.Peak;
 
 public class Fragmenter {
 	private static final Map<Character, Double> mapMasses = new HashMap<>();
@@ -42,7 +43,106 @@ public class Fragmenter {
 		return masses;
 	}
 	
-	public void addBIons() {
+	private void addIons( double baseMass, int charge, int pos, IonType base, IonType h20, IonType nh3 ) {
+		FragmentIon ion = new FragmentIon();
+		ion.setCharge(charge);
+		ion.setMzExp(baseMass/charge);
+		ion.setMzError(0.0);
+		ion.setIndex(pos);
+		ion.setType(base);
+		totalIons.add(ion);
 		
+		ion = new FragmentIon();
+		ion.setCharge(charge);
+		ion.setMzExp((baseMass-Masses.H2O)/charge);
+		ion.setMzError(0.0);
+		ion.setIndex(pos);
+		ion.setType(h20);
+		totalIons.add(ion);
+		
+		ion = new FragmentIon();
+		ion.setCharge(charge);
+		ion.setMzExp((baseMass-Masses.NH3)/charge);
+		ion.setMzError(0.0);
+		ion.setIndex(pos);
+		ion.setType(nh3);
+		totalIons.add(ion);
+	}
+	
+	public void addPrecursorIons() {
+		double mh = psm.getCalcMz()*psm.getCharge();
+		for( int charge = 1; charge <= psm.getCharge(); charge++ )
+			addIons(mh+(charge-1)*Masses.Hydrogen, charge, 0, IonType.PRECURSOR, IonType.PRECURSOR_H2O, IonType.PRECURSOR_NH3);
+	}
+	
+	public void addAIons() {
+		throw new UnsupportedOperationException("a ions not supported");
+	}
+	
+	public void addBIons() {
+		int len = psm.getPeptide().getSequence().length();
+		for( int charge = 1; charge <= psm.getCharge(); charge++ )
+			for( int i = 0; i < len; i++ ) {
+				double mass = 0.0;                
+                for( int j = 0; j <= i; j++ )
+                	mass += partialMasses[j];
+                addIons(mass+charge*Masses.Hydrogen, charge, i+1, IonType.B, IonType.B_H2O, IonType.B_NH3);
+			}
+	}
+	
+	public void addCIons() {
+		throw new UnsupportedOperationException("c ions not supported");
+	}
+	
+	public void addDIons() {
+		throw new UnsupportedOperationException("d ions not supported");
+	}
+	
+	public void addVIons() {
+		throw new UnsupportedOperationException("v ions not supported");
+	}
+	
+	public void addWIons() {
+		throw new UnsupportedOperationException("w ions not supported");
+	}
+	
+	public void addXIons() {
+		throw new UnsupportedOperationException("x ions not supported");
+	}
+	
+	public void addYIons() {
+		int len = psm.getPeptide().getSequence().length();
+		for( int charge = 1; charge <= psm.getCharge(); charge++ )
+			for( int i = 0; i < len; i++ ) {
+				double mass = 0.0;                
+                for( int j = 0; j <= i; j++ )
+                	mass += partialMasses[len-1-j];
+                mass += Masses.C_term+Masses.Hydrogen;
+                addIons(mass+charge*Masses.Hydrogen, charge, i+1, IonType.Y, IonType.Y_H2O, IonType.Y_NH3);
+			}
+	}
+	
+	public void addZIons() {
+		throw new UnsupportedOperationException("z ions not supported");
+	}
+	
+	public List<FragmentIon> match( double mzError ) {
+		List<FragmentIon> results = new ArrayList<>();
+		for( FragmentIon ion : totalIons )
+			for( Peak peak : psm.getSpectrum().getPeaks() ) {
+				double error = peak.getMz()-ion.getMzCalc();
+				if( error >= -mzError && error <= mzError ) {
+					FragmentIon result = new FragmentIon();
+					result.setCharge(ion.getCharge());
+					result.setIndex(ion.getIndex());
+					result.setType(ion.getType());					
+					result.setMzExp(peak.getMz());
+					result.setMzError(error);
+					result.setIntensity(peak.getIntensity());
+					results.add(result);					
+					break;
+				}
+			}
+		return results;
 	}
 }
