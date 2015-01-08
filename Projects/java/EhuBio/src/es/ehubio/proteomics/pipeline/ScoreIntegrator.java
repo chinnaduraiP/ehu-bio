8c988632820f3df9ca076292e3597df71fd83d06
gorka.prieto@gmail.com <gorka.prieto@gmail.com@f831f5fe-2d2f-98d9-2631-73cc1f612daa>
2015-01-08 16:27:47 +0000

    Updated ScoreIntegrator

--- Projects/java/EhuBio/src/es/ehubio/proteomics/pipeline/ScoreIntegrator.java
+++ Projects/java/EhuBio/src/es/ehubio/proteomics/pipeline/ScoreIntegrator.java
@@ -3,6 +3,7 @@ package es.ehubio.proteomics.pipeline;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import es.ehubio.proteomics.DecoyBase;
 import es.ehubio.proteomics.Peptide;
@@ -26,7 +27,7 @@ public class ScoreIntegrator {
 	public static void updatePeptideScores( Collection<Peptide> peptides ) {
 		for( Peptide peptide : peptides ) {
 			basicIntegrator(
-				peptide.getPsms(), ScoreType.PSM_P_VALUE, ScoreType.PSM_SPHPP_SCORE,
+				peptide.getPsms(), ScoreType.PSM_SPHPP_SCORE,
 				peptide, ScoreType.PEPTIDE_P_VALUE, ScoreType.PEPTIDE_SPHPP_SCORE);
 		}
 	}
@@ -47,9 +48,12 @@ public class ScoreIntegrator {
 	 * @param proteins
 	 * @param decoyPrefix the decoy accession should be this prefix followed by the target accession.
 	 */
-	public static void updateProteinScoresAprox( Collection<Protein> proteins, String decoyPrefix ) {
+	public static void updateProteinScoresPrefix( Collection<Protein> proteins, String decoyPrefix ) {
 		updateProteinScoresBasic(proteins);
-		
+		normalizeProteinScores(proteins, decoyPrefix);
+	}
+	
+	private static void normalizeProteinScores( Collection<Protein> proteins, String decoyPrefix ) {
 		Map<String, Protein> mapDecoys = new HashMap<>();
 		for( Protein protein : proteins )
 			if( Boolean.TRUE.equals(protein.getDecoy()) )
@@ -69,26 +73,79 @@ public class ScoreIntegrator {
 			score.setValue(score.getValue()/N);
 			protein.setScore(new Score(ScoreType.PROTEIN_P_VALUE, Math.exp(-score.getValue())));
 		}
-	}	
+	}
+	
+	public static int updateProteinScoresIter( Collection<Protein> proteins, String decoyPrefix, double epsilon, int maxIters ) {
+		Map<Protein, Map<Peptide,Double>> mapFactors = initFactors(proteins);
+		int iteration = 0;
+		while( iteration < maxIters && updateProteinScoresStep(proteins,mapFactors,epsilon) )
+			logger.info(String.format("Iteration = %d",++iteration));
+		normalizeProteinScores(proteins, decoyPrefix);
+		return iteration;
+	}
+	
+	private static Map<Protein, Map<Peptide,Double>> initFactors( Collection<Protein> proteins ) {
+		Map<Protein, Map<Peptide,Double>> mapFactors = new HashMap<>();
+		for( Protein protein : proteins ) {
+			double score = 0.0;
+			Map<Peptide,Double> scores = new HashMap<>();
+			for( Peptide peptide : protein.getPeptides() ) {
+				double factor = 1.0/peptide.getProteins().size();
+				scores.put(peptide, factor);
+				score += factor*peptide.getScoreByType(ScoreType.PEPTIDE_SPHPP_SCORE).getValue();
+			}
+			protein.setScore(new Score(ScoreType.PROTEIN_SPHPP_SCORE, score));
+			mapFactors.put(protein, scores);
+		}
+		return mapFactors;
+	}
+	
+	private static boolean updateProteinScoresStep( Collection<Protein> proteins, Map<Protein, Map<Peptide,Double>> mapFactors, double epsilon ) {
+		updateFactors(proteins, mapFactors);
+		
+		boolean changed = false;		
+		for( Protein protein : proteins ) {
+			double newScore = 0.0;
+			for( Peptide peptide : protein.getPeptides() )
+				newScore += mapFactors.get(protein).get(peptide)*peptide.getScoreByType(ScoreType.PEPTIDE_SPHPP_SCORE).getValue();
+			Score score = protein.getScoreByType(ScoreType.PROTEIN_SPHPP_SCORE);			
+			if( !changed && Math.abs(newScore-score.getValue()) > epsilon )
+				changed = true;
+			score.setValue(newScore);
+		}
+		
+		return changed;
+	}
+
+	private static void updateFactors( Collection<Protein> proteins, Map<Protein, Map<Peptide,Double>> mapFactors ) {
+		for( Protein protein : proteins ) {
+			double num = protein.getScoreByType(ScoreType.PROTEIN_SPHPP_SCORE).getValue();
+			for( Peptide peptide : protein.getPeptides() ) {
+				double den = 0.0;
+				for( Protein protein2 : peptide.getProteins() )
+					den += protein2.getScoreByType(ScoreType.PROTEIN_SPHPP_SCORE).getValue();				
+				mapFactors.get(protein).put(peptide, num/den);
+			}
+		}
+	}
 	
 	public static void updateGroupScoresBasic( Collection<ProteinGroup> groups ) {
 		for( ProteinGroup group : groups ) {
 			basicIntegrator(
-				group.getProteins(), ScoreType.PROTEIN_P_VALUE, ScoreType.PROTEIN_SPHPP_SCORE,
+				group.getProteins(), ScoreType.PROTEIN_SPHPP_SCORE,
 				group, ScoreType.GROUP_P_VALUE, ScoreType.GROUP_SPHPP_SCORE);
 		}
 	}
 	
-	private static void basicIntegrator(Collection<? extends DecoyBase> subitems, ScoreType lowP, ScoreType lowS, DecoyBase item, ScoreType upP, ScoreType upS) {
-		double p = 1.0;
+	private static void basicIntegrator(Collection<? extends DecoyBase> subitems, ScoreType lowS, DecoyBase item, ScoreType upP, ScoreType upS) {
 		double s = 0.0;
-		for( DecoyBase subitem : subitems ) {
-			p *= subitem.getScoreByType(lowP).getValue();
+		for( DecoyBase subitem : subitems )
 			s += subitem.getScoreByType(lowS).getValue();
-		}
-		Score pValue = new Score(upP,p);
+		Score pValue = new Score(upP,Math.exp(-s));
 		Score spHpp = new Score(upS,s);
 		item.setScore(pValue);
 		item.setScore(spHpp);
-	}	
+	}
+	
+	private static final Logger logger = Logger.getLogger(ScoreIntegrator.class.getName());
 }
