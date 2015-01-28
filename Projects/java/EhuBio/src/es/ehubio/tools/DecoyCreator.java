8583b3041c37a59a3f4dc53803732cd2427866fb
gorka.prieto@gmail.com <gorka.prieto@gmail.com@f831f5fe-2d2f-98d9-2631-73cc1f612daa>
2015-01-28 19:21:09 +0000

    Wregex - Included NESdb in target list - Solved minor issue
    Proteomics
    - Enzyme is now a parameter in CLI of DecoyCreator

--- Projects/java/EhuBio/src/es/ehubio/tools/DecoyCreator.java
+++ Projects/java/EhuBio/src/es/ehubio/tools/DecoyCreator.java
@@ -7,23 +7,22 @@ public class DecoyCreator implements Command.Interface {
 
 	@Override
 	public String getUsage() {
-		return "<target.fasta> <decoy.fasta> <prefix>";
+		return "<target.fasta> <decoy.fasta> <enzyme> <prefix>";
 	}
 
 	@Override
 	public int getMinArgs() {
-		return 3;
+		return 4;
 	}
 
 	@Override
 	public int getMaxArgs() {
-		return 3;
+		return 4;
 	}
 
 	@Override
-	public void run(String[] args) throws Exception {
-		//DecoyDb.create(args[0], args[1], DecoyDb.Strategy.PSEUDO_REVERSE, Enzyme.TRYPSIN_P, args[2]);
-		DecoyDb.create(args[0], args[1], DecoyDb.Strategy.PSEUDO_REVERSE, Enzyme.TRYPSIN, args[2]);		
+	public void run(String[] args) throws Exception {		
+		DecoyDb.create(args[0], args[1], DecoyDb.Strategy.PSEUDO_REVERSE, Enzyme.valueOf(args[2]), args[3]);		
 	}
 
 }
