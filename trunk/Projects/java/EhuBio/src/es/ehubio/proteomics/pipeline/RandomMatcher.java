f8c14e13cca14c49b4b9d6b94483a5fe22f1984d
gorka.prieto@gmail.com <gorka.prieto@gmail.com@f831f5fe-2d2f-98d9-2631-73cc1f612daa>
2015-02-19 19:10:05 +0000

    Proteomics - Included expectad and observed Nq and Mq values as protein scores

--- trunk/Projects/java/EhuBio/src/es/ehubio/proteomics/pipeline/RandomMatcher.java
+++ trunk/Projects/java/EhuBio/src/es/ehubio/proteomics/pipeline/RandomMatcher.java
@@ -3,5 +3,23 @@ package es.ehubio.proteomics.pipeline;
 import es.ehubio.proteomics.Protein;
 
 public interface RandomMatcher {
-	double getExpected( Protein protein );
+	
+	public static class Result {				
+		public Result(double Nq, double Mq) {
+			this.Nq = Nq;
+			this.Mq = Mq;
+		}
+		
+		public double getNq() {
+			return Nq;
+		}
+		
+		public double getMq() {
+			return Mq;
+		}
+		
+		private final double Nq, Mq;
+	}
+	
+	Result getExpected( Protein protein );
 }
